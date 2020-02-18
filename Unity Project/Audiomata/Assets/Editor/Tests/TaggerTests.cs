d48f4c086ef290792fa5b37d2747cb6ea992ae84
pharezvitalis <pharez.vitalis@students.plymouth.ac.uk>
2020-02-18 15:58:25 +0000

    Finished first iteration of class that manages tagging audio

--- Unity Project/Audiomata/Assets/Editor/Tests/TaggerTests.cs
+++ Unity Project/Audiomata/Assets/Editor/Tests/TaggerTests.cs
@@ -1,19 +1,126 @@
 ﻿using NUnit.Framework;
 using System.Collections;
-
+using System.IO;
+using UnityEngine;
+//
+[TestFixture]
 public class TaggerTests
 {
-     string[] trackNames = { "Bassy Explosion", "CC Rock Beat" };
+    string[] trackNames = { "Bassy Explosion", "CC Rock Beat" };
 
     [Test]
     public void TestFindAllAssets()
     {
         TrackTagger tagger = new TrackTagger();
 
-        string[] receivedTrackNames = tagger.GetAllAudio();
+        string[] receivedTrackNames = tagger.GetAudioAssets();
         for (int i = 0; i < trackNames.Length; i++)
         {
             Assert.That(receivedTrackNames, Has.Exactly(1).EqualTo(trackNames[i]));
         }
     }
+
+    [Test]
+    public void ActiveTracksTest()
+    {
+        TrackTagger tagger = new TrackTagger();
+        string[] receivedTracks = tagger.GetActiveTrackNames();
+        for (int i = 0; i < trackNames.Length; i++)
+        {
+            Assert.That(receivedTracks, Has.Exactly(1).EqualTo(trackNames[i]));
+        }
+    }
+
+    
+    [TestCase("boom", 0)]
+    [TestCase("drums", 1)]
+    public void AddTrackTest(string tag = "test", int trackNameIdx = 0)
+    {
+        TrackTagger tagger = new TrackTagger();
+
+        tagger.TagTrack(tag, trackNames[trackNameIdx]);
+
+        Assert.That(tagger.GetTrack(tag, out var track));
+        Assert.That(track, Is.EqualTo(trackNames[trackNameIdx]));
+    }
+    
+    [TestCase("epic dramatic classical")]
+    public void AddTagTest(string tagsIn)
+    {
+        TrackTagger tagger = new TrackTagger();
+    string[] tags = tagsIn.Split(' ');
+
+        for (int i = 0; i < tags.Length; i++)
+        {
+            tagger.AddTag(tags[i]);
+        }
+
+        string[] resultTags = tagger.GetAllTags();
+
+        Assert.That(resultTags.Length == tags.Length);
+
+        for (int i = 0; i < resultTags.Length; i++)
+        {
+            Assert.That(resultTags, Has.Exactly(1).EqualTo(tags[i]));
+        }
+    }
+
+    [TestCase("epic dramatic classical")]
+    public void RemoveTag(string tagsIn)
+    {
+        TrackTagger tagger = new TrackTagger();
+
+        string[] tags = tagsIn.Split(' ');
+
+        for (int i = 0; i < tags.Length; i++)
+        {
+            tagger.AddTag(tags[i]);
+            tagger.RemoveTag(tags[i]);
+        }
+        tagger.RemoveTag("Fake Tag");
+
+        Assert.That(tagger.GetAllTags().Length == 0);
+    }
+    
+    [TestCase("test.json")]
+    public void SaveLoadTest(string fileName)
+    {
+        TrackTagger tagger = new TrackTagger();
+
+        string path = Application.dataPath + "\\" + fileName;
+        string[] tags = { "great", "emotional", "loud", "classical" };
+
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
+
+        tagger.TagTrack(tags[0], trackNames[0]);
+        tagger.TagTrack(tags[1], trackNames[0]);
+        tagger.TagTrack(tags[2], trackNames[0]);
+        tagger.TagTrack(tags[2], trackNames[1]);
+        tagger.TagTrack(tags[3], trackNames[1]);
+        tagger.TagTrack(tags[1], trackNames[1]);
+
+        tagger.SaveAll(path);
+
+        Assert.That(File.Exists(path));
+
+        tagger.Load(path);
+
+        string[] resultTags = tagger.GetAllTags();
+        string[] resultTracks = tagger.GetActiveTrackNames();
+
+        for (int i = 0; i < resultTags.Length; i++)
+        {
+            Assert.That(resultTags, Has.Exactly(1).EqualTo(tags[i]));
+        }
+
+        for (int i = 0; i < resultTracks.Length; i++)
+        {
+            Assert.That(resultTracks, Has.Exactly(1).EqualTo(trackNames[i]));
+        }
+    }
+
+    
 }
--- Unity Project/Audiomata/Assets/Editor/TrackTagger.cs
+++ Unity Project/Audiomata/Assets/Editor/TrackTagger.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 /// <summary>
 /// Class manages the tagging of Audio Clips
 /// </summary>
 class TrackTagger
 {
+    /// <summary>
+    /// Dictionary of all tags against tracks
+    /// </summary>
+    Dictionary<string, List<string>> taggedTracks;
 
     /// <summary>
-    /// Gets all Audio Clips from within the project
+    /// clip name to asset ID
+    /// </summary>
+    Dictionary<string, string> cliptoGUID;
+    
+
+    public TrackTagger()
+    {
+        taggedTracks = new Dictionary<string, List<string>>();
+        cliptoGUID = new Dictionary<string, string>();
+        GetAudioAssets();
+    }
+
+    /// <summary>
+    /// Gets all Audio Clips from within the project updates dictionary of tracks where appropriate
     /// </summary>
     /// <returns>an array of all audio clip names from within the project </returns>
-    public  string[] GetAllAudio()
+    public  string[] GetAudioAssets()
     {
         string[] foundAssets = AssetDatabase.FindAssets("t:AudioClip");
 
@@ -27,11 +47,145 @@ class TrackTagger
             nextClip = nextClip.Remove(dotIdx, nextClip.Length-dotIdx);
 
             trackNames[i] = nextClip;
+            if (!cliptoGUID.ContainsKey(nextClip))
+            {
+                cliptoGUID.Add(nextClip, foundAssets[i]);
+            }
+            
         }
-
-
         return trackNames;
     }
 
+    public string[] GetActiveTrackNames()
+    {
+        string[] tracks = new string[cliptoGUID.Count];
+        cliptoGUID.Keys.CopyTo(tracks, 0);
+        return tracks;
+    }
+
+    public void TagTrack(string tag, string trackName)
+    {
+        if (taggedTracks.TryGetValue(tag, out var trackList))
+        {
+            trackList.Add(trackName);
+        }
+        else
+        {
+            trackList = new List<string>();
+            trackList.Add(trackName);
+            taggedTracks.Add(tag, trackList);
+        }
+    }
+
+   public bool GetTrack(string tag, out string trackName)
+    {
+        if(!taggedTracks.TryGetValue(tag, out var tracks))
+        {
+            trackName = null;
+            return false;
+        }
+
+        trackName = tracks[Random.Range(0, tracks.Count)];
+        return true;
+    }
+
+    public void AddTag(string tag)
+    {
+        if (!taggedTracks.ContainsKey(tag))
+        {
+            taggedTracks.Add(tag, new List<string>());
+        }
+    }
+
+    public void RemoveTag(string tag)
+    {
+        taggedTracks.Remove(tag);
+    }
+
+   public string[] GetAllTags()
+    {
+        string[] tags = new string[taggedTracks.Count];
+        taggedTracks.Keys.CopyTo(tags, 0);
+
+        return tags;
+    }
+
+    public void SaveAll(string path)
+    {
+        List<TaggedTrack> trackData  = new List<TaggedTrack>();
+
+        foreach (var clip in cliptoGUID)
+        {
+            trackData.Add(new TaggedTrack() { name = clip.Key,tags= new List<string>() });
+        }
+
+        foreach(var entry in taggedTracks)
+        {
+            for (int i = 0; i < trackData.Count; i++)
+            {
+                if (entry.Value.Contains(trackData[i].name))
+                {
+                    trackData[i].tags.Add(entry.Key);
+                }
+            }
+        }
+
+        try
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(trackData));
+        }
+        catch (IOException e)
+        {
+            Debug.Log("Audiomata: Failed To Save Tags: "+e.Message);
+        }
+    }
+
+    public void Load(string path, bool clearAll = true)
+    {
+        if (clearAll)
+        {
+            taggedTracks.Clear();
+        }
+        string data;
+        try
+        {
+             data = File.ReadAllText(path);
+        }
+        catch(IOException e)
+        {
+            Debug.Log("Audiomata: Failed to load Tag file: " + e.Message);
+            return;
+        }
+        
+
+       JArray arr = JArray.Parse(data);
+        for (int i = 0; i < arr.Count; i++)
+        {
+            TaggedTrack next = JsonConvert.DeserializeObject<TaggedTrack>(arr[i].ToString());
+
+
+            for (int j = 0; j < next.tags.Count; j++)
+            {
+                string nextTag = next.tags[j];
+                if (!taggedTracks.ContainsKey(nextTag))
+                {
+                    List<string> trackNames = new List<string>();
+                    trackNames.Add(next.name);
+                    taggedTracks.Add(nextTag, trackNames);
+                }
+                else
+                {
+                    taggedTracks[nextTag].Add(next.name);
+                }
+                
+            }
+        }
+    }
  }
+
+public struct TaggedTrack
+{
+   public string name;
+    public List<string> tags;
+}
 
